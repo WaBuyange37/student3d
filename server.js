12c493176e15bceca890f8d9e7de4a043530c3fe
WaBuyange37 <nzelabeatblack37@gmail.com>
2025-04-20 13:04:09 +0200

    db mysql connection done

---
+++ server.js
@@ -0,0 +1,29 @@
+require('dotenv').config(); 
+
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors')
+const app = express();
+
+app.use(cors);
+app.use(express.json());
+// connekt mysql
+const db = mysql.createConnection({
+  host: process.env.DB_HOSTMe,
+  user: process.env.DB_USERMe,
+  password: process.env.DB_PASSWORDMe,
+  database: process.env.DB_NAMEMe,
+});
+db.connect(err =>{
+  if(err) throw err;
+  console.log('mysql connected');
+});
+
+
+
+
+
+
+
+
+
